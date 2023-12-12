f8aad61453ce722e8ff3a9913f4887c3a950bcea
ALGRACE123 <salmanmarolix@gmail.com>
2023-12-13 04:00:24 +0530

    13th dec

--- src/test/java/Module2/MathClass.java
+++ src/test/java/Module2/MathClass.java
@@ -3,10 +3,11 @@ package Module2;
 public class MathClass {
 
 	public static void main(String[] args) {
-		int num=4;
-		System.out.println(Math.pow(num,2));
+		int num = 4;
+		System.out.println(Math.pow(num, 2));
 		System.out.println(Math.max(10, 39));
 		System.out.println(Math.sqrt(36));
-		}
-
+		System.out.println(Math.PI);
 	}
+
+}
